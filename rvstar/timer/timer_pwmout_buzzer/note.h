a3bb91dea580a24cedb7b31e241973aee1374725
Young <72432285+xuyoung2020@users.noreply.github.com>
2021-05-20 14:20:21 +0800

    rvstar: add a timer pwm example that uses a buzzer to play music (#6)
    * lab: add a buzzer example for rvstar
    
    * Update README.md
    
    * correct the PB1 pin
    
    Co-authored-by: CasPaPaPa <zeonphantom@163.com>

---
+++ rvstar/timer/timer_pwmout_buzzer/note.h
@@ -0,0 +1,24 @@
+#define NTD0 -1
+#define NTD1 293
+#define NTD2 329
+#define NTD3 368
+#define NTD4 390
+#define NTD5 438
+#define NTD6 492
+#define NTD7 554
+
+#define NTDL1 147
+#define NTDL2 166
+#define NTDL3 185
+#define NTDL4 196
+#define NTDL5 221
+#define NTDL6 248
+#define NTDL7 278
+
+#define NTDH1 585
+#define NTDH2 657
+#define NTDH3 700
+#define NTDH4 781
+#define NTDH5 882
+#define NTDH6 990
+#define NTDH7 1112
